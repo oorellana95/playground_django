900b3e16b05e39cadf03c9634ffffacce5c8b36b
oorellana95 <oorellana30.95@gmail.com>
2023-08-08 09:29:25 -0500

    rename project

--- playground_django/urls.py
+++ playground_django/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for storefront_django project.
+URL configuration for playground_django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
