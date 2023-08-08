73b4b37066cc35e4451e9ddb5cf98abf3dd0a998
oorellana95 <oorellana30.95@gmail.com>
2023-08-08 10:24:10 -0500

    register topic and message to the admin panel

--- studybud/admin.py
+++ studybud/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Room
+from .models import Room, Topic, Message
 admin.site.register(Room) #used to interact with the table in the admin panel
+admin.site.register(Topic)
+admin.site.register(Message)
