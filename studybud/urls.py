7d7e24eaa5e0e6562b53d00b5816363563f48ae1
oorellana95 <oorellana30.95@gmail.com>
2023-08-13 22:10:03 -0500

    add room template and messages

--- studybud/urls.py
+++ studybud/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path(route='logout/', view=views.logoutUser, name="logout"),
     path(route='register/', view=views.registerPage, name="register"),
     path(route='home/', view=views.home, name="home"),
-    path(route='rooms/<int:pk>/', view=views.room, name="rooms"),
+    path(route='rooms/<int:pk>/', view=views.room, name="room"),
     path('create-room/', views.create_room, name="create-room"),
     path('update-room/<int:pk>', views.update_room, name="update-room"),
     path('delete-room/<int:pk>', views.delete_room, name="delete-room")
--- studybud/views.py
+++ studybud/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 def loginPage(request):
@@ -36,7 +35,6 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -53,21 +51,30 @@ def registerPage(request):
     return render(request, 'login_register.html', context)
 
 def home(request):
+    topics = Topic.objects.all()
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) | 
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
-    topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms':rooms, 'topics': topics, 'room_count': room_count} 
+    context = {'topics': topics, 'rooms':rooms, 'room_count': room_count} 
     return render(request=request, template_name='home.html', context=context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'rooms':[room]} 
-    return render(request=request, template_name='home.html', context=context)
+    if request.method == 'POST':
+        Message.objects.create(
+            user=request.user,
+            room=room,
+            body= request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+    
+    room_messages = room.message_set.all()
+    context = {'room':room, 'room_messages': room_messages} 
+    return render(request=request, template_name='room.html', context=context)
 
 @login_required(login_url='/login')
 def create_room(request):
@@ -109,4 +116,5 @@ def delete_room(request, pk):
     if request.method == "POST":
         room.delete()
         return redirect('home')
+    
     return render(request, 'delete.html', {'obj':room})
