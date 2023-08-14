4428747d95171f77ecfd92050a036890a162120e
oorellana95 <oorellana30.95@gmail.com>
2023-08-13 19:03:59 -0500

    Add topic sidebar filter

--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
-Refreshing Python Django
+Refreshing Python Django following the next video:
 Link: https://www.youtube.com/watch?v=rHux0gMZ3Eg
--- studybud/views.py
+++ studybud/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms} 
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__icontains=q)
+    topics = Topic.objects.all()
+    context = {'rooms':rooms, 'topics': topics} 
     return render(request=request, template_name='home.html', context=context)
 
 def room(request, pk):
