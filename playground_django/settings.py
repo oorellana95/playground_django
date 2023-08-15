5d111c1fea47fe0e8cfdaefc0aa8716cbe769d0a
oorellana95 <oorellana30.95@gmail.com>
2023-08-14 21:10:20 -0500

    fix cors

--- playground_django/settings.py
+++ playground_django/settings.py
@@ -38,12 +38,15 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "playground",
-    "studybud"
+    "studybud",
+    "corsheaders"
     #"debug_toolbar",
 ]
 
 MIDDLEWARE = [
     #"debug_toolbar.middleware.DebugToolbarMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -133,3 +136,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CORS_ALLOW_ALL_ORIGINS = True
